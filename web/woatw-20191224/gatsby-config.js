e3861f933785474389617c95467414ad3ddb3d65
Rob Brennan <rob@therobbrennan.com>
2019-12-24 17:54:25 -0800

    Added gatsby-plugin-gtag

--- web/woatw-20191224/gatsby-config.js
+++ web/woatw-20191224/gatsby-config.js
@@ -12,6 +12,17 @@ module.exports = {
     siteUrl: "https://www.woatw.com"
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-68343119-6`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     // Add gatsby-theme-musician to your site
     {
       resolve: "gatsby-theme-musician",
