9b5d7b86d5ca4633c613c81e9aae3bda78f3893e
Rob Brennan <rob@therobbrennan.com>
2020-01-14 23:42:45 -0800

    Added gatsby-plugin-manifest configuration details

--- web/woatw-20191224/gatsby-config.js
+++ web/woatw-20191224/gatsby-config.js
@@ -26,6 +26,26 @@ module.exports = {
       },
     },
 
+    // Add favicon and other PWA manifest config
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Wyatt Olney & The Wreckage",
+        short_name: "WOATW",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "content/images/favicon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+
     // Add gatsby-theme-musician to your site
     {
       resolve: "gatsby-theme-musician",
