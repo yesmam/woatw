27a951bdfe4d20f2fb3422d74a563464a08109ba
Rob Brennan <rob@therobbrennan.com>
2020-01-15 13:45:15 -0800

    Replaced default Banner component with an exact copy of the source files from the original author

---
+++ web/woatw-20191224/components/fix-for-locale-display-bug-in-theme/banner.js
@@ -0,0 +1,84 @@
+/** @jsx jsx */
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import PropTypes from "prop-types"
+import { Styled, jsx, Container } from "theme-ui"
+
+import useSiteMetadata from "../use-site-metadata"
+import HeroImage from "./hero-image"
+import Social from "./social"
+
+let additionalStyles = {}
+let bgOverlayStyles = {}
+
+const BannerContent = ({ title, tagline }) => (
+  <>
+    <Styled.h1>{title}</Styled.h1>
+    {tagline && <p>{tagline}</p>}
+    <Social />
+  </>
+)
+
+const BannerV2 = ({ children, bgOverlay, color }) => {
+  const { title, artist, bannerImg } = useSiteMetadata()
+
+  if (bannerImg) {
+    additionalStyles["flexDirection"] = "column"
+  }
+  if (color) {
+    additionalStyles["color"] = "color"
+  }
+
+  if (bgOverlay) {
+    bgOverlayStyles = {
+      "&:after": {
+        background: bgOverlay,
+      },
+    }
+  }
+
+  const bannerContentElement = (
+    <BannerContent
+      title={title}
+      tagline={typeof artist.tagline === "undefined" ? null : artist.tagline}
+    />
+  )
+
+  return (
+    <div
+      className="GtmBanner"
+      sx={{
+        variant: "components.banner",
+        ...additionalStyles,
+        ...bgOverlayStyles,
+      }}
+    >
+      {bannerImg ? (
+        <HeroImage
+          className="GtmBanner__hero-wrapper"
+          fluid={bannerImg.fluid}
+          sx={{ flexGrow: 1 }}
+        >
+          <Container className="GtmBanner__content-wrapper">
+            {children || bannerContentElement}
+          </Container>
+        </HeroImage>
+      ) : (
+        <Container
+          className="GtmBanner__content-wrapper"
+          sx={{ alignSelf: "center" }}
+        >
+          {children || bannerContentElement}
+        </Container>
+      )}
+    </div>
+  )
+}
+
+BannerV2.propTypes = {
+  children: PropTypes.any,
+  bgOverlay: PropTypes.string,
+  color: PropTypes.string,
+}
+
+export default BannerV2
